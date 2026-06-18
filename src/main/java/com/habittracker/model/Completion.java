0fbf0fddfd04a7191ee77bee095f1895c974e3df
MSwally <swallace@gmx.at>
2026-06-18 17:34:54 +0200

    Create Habit and Completion class

--- src/main/java/com/habittracker/model/Completion.java
+++ src/main/java/com/habittracker/model/Completion.java
@@ -1,5 +1,35 @@
 package com.habittracker.model;
 
+import java.time.LocalDateTime;
+
 public class Completion {
-    
+    private String habitId;
+    private String dateTime;
+
+    // Constructors - called when creating a new instance of the Completion class
+    public Completion(String habitId, LocalDateTime dateTime) {
+        this.habitId = habitId;
+        this.dateTime = dateTime.toString();
+    }
+
+    // Empty Constructor for Gson
+    public Completion() {}
+
+    // Getters for external reading
+    public String getHabitId() {
+        return habitId;
+    }
+
+    public LocalDateTime getDateTime() {
+        return LocalDateTime.parse(dateTime);
+    }
+
+    // Setters for external writing
+    public void setHabitId(String habitId) {
+        this.habitId = habitId;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
 }
--- src/main/java/com/habittracker/model/Habit.java
+++ src/main/java/com/habittracker/model/Habit.java
@@ -1,5 +1,45 @@
 package com.habittracker.model;
 
+import java.util.UUID;
+
 public class Habit {
-    
+    private String id;
+    private String name;
+    private String frequency;
+
+    // Constructors - called when creating a new instance of the Habit class
+    public Habit(String name, String frequency) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.frequency = frequency;
+    }
+
+    // Empty Constructor for Gson
+    public Habit() {}
+
+    // Getters for external reading
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFrequency() {
+        return frequency;
+    }
+
+    // Setters for external writing
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
 }
